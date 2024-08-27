7b546c48136c6cde67b31ce8fd2737a6cef02e71
Matt Shaw <Shaw-M18@ulster.ac.uk>
2024-08-27 18:37:49 +0100

    laptop acting up so committing what this index blade page incase it crashes mid way through

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang ="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Task List</title>
+<style>
+    body {
+        font-family: Helvetica, Arial, sans-serif;
+        background-color: #005cbf;
+        margin: 0;
+        padding: 0;
+    }
+    .container {
+        max-width: 800px; /*make sure the structure is correct ad that nothing goes off the screen*/
+        margin: 20px auto;
+        padding: 20px;
+        background-color: #ffffff; /* basic white background*/
+        border-radius: 5px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); /*Mental not to remember to check if colour is suitable to the new blue i picked*/
+    }
+    h1 {
+    font-size: 26px;
+    color: #000000;
+    }
+
+
+    </style>
+</head>
