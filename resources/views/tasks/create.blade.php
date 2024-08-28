ecc30b12bf74a0435f9930a4e1e9a2365de1da60
Matt Shaw <Shaw-M18@ulster.ac.uk>
2024-08-28 23:31:26 +0100

    Beginning of create blade file

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+    <title>Create Task</title>
+    <style>
+     body {
+         font-family: Helvetica, Arial, sans-serif;
+         background-color: #005cbf;
+         margin: 0;
+         padding: 0;
+     }
+     .container {
+
+     }
+
+
+    </style>
+
+
+
+
+</head>
+</html>
