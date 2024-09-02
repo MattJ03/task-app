3a91929e2f05b0294aa57ad659c525a54374e972
Matt Shaw <Shaw-M18@ulster.ac.uk>
2024-09-02 11:58:01 +0100

    adding a route which will take me straight to the index page rather than having to manual change the local host url each time i check during dev

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 // should map to all needed pages and complte CRUD from TC
+route::get('/', [TaskController::class, 'index']);
 Route::resource('tasks', TaskController::class);
