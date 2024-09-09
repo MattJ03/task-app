5a52f132bc46944322120416fb2dac02ceaf8601
Matt Shaw <Shaw-M18@ulster.ac.uk>
2024-09-09 19:45:41 +0100

    further work on the show page

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -2,8 +2,17 @@
     <div class="container">
         <h1> {{$task->title}}</h1>  //heading based on selected individual task
 
-        <p><strong>Description:</strong> {{$task->description ?? 'No description'}}</p>
+        <p><strong>Description:</strong> {{$task->description ?? 'No description given'}}</p>
+        <p><strong>Time to complete</strong> {{$task->time_to_complete }} minutes</p>
+        <p><strong>Completed</strong> {{$task->completed}} ? 'Yes' : 'No'</p>
 
+        //button to edit the task
+        <a href="route {{'tasks.edit', $task->id }}" class="btn btn-primary">Edit Task</a>
+        <form action="route {{'tasks.destroy', $task->id }}" method="POST" style="display: inline;">
+            @csrf
+            @method('DELETE');
+            <button class="btn btn-primary">Delete Task</button>
+        </form>
 
     </div>
 </x-layouts.app>
