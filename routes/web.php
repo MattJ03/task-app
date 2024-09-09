75035ac2aff8fd9037d440a4dae99b12884f7823
Matt Shaw <Shaw-M18@ulster.ac.uk>
2024-09-09 22:57:54 +0100

    inatalled laravel breeze for authentication

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,31 @@
 <?php
-use App\Http\Controllers\TaskController;
+
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-// Define the root route to redirect to tasks.index
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
 Route::get('/', function () {
-return redirect()->route('tasks.index');
+    return view('welcome');
 });
 
-// Resource route for TaskController
- Route::resource('tasks', TaskController::class);
-  //Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
-  //Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
-  //Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
-  //Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
-  //Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
-  //Route::patch('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
- // Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
